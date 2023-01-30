334c5158c8fba66a8859b928cdf1e5340410857d
BagaBaga <bdesign@bk.ru>
2023-01-30 04:08:33 +0300

    "Не понял что в deth8 писать"

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -2,6 +2,12 @@ package koschei.models;
 
 public class Egg6 {
 
+    private Needle7 needle7;
+
+    public Egg6(Needle7 needle7) {
+        this.needle7 = needle7;
+    }
+
     @Override
     public String toString() {
         return ", в яйце иголка " + "";
